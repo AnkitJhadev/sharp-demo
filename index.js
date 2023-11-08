dfa5a32f64caa56fbef25f8bd9eaf45223169c9d
AnkitJhadev <jha80250@gmail.com>
2023-11-08 18:42:49 +0530

    This is my second commit

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 // console.log(document.timeline);
 // const title = document.getElementById("main-header");
 // title.innerText = 'HELLO' ;
-const header = document.getElementById("header-title");
-header.textContent = 'LIST OF ITEMS';
-const change = document.getElementsByClassName("title");
-change.textContent = "Add List Of Items";
-header.style.color="red";
+// const header = document.getElementById("header-title");
+// header.textContent = 'LIST OF ITEMS';
+// const change = document.getElementsByClassName("title");
+// change.textContent ;= "Add List Of Items";
+// header.style.color="red"
+var list = document.getElementsByClassName('list-group');
+list.getElementsByClassName('list-group').innerText = 'hello';
