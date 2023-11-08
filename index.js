72df14be0bfa683186c88762c33bd7f29731ad91
AnkitJhadev <jha80250@gmail.com>
2023-11-08 18:15:38 +0530

    This is sharpener project

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.timeline);
+// const title = document.getElementById("main-header");
+// title.innerText = 'HELLO' ;
+const header = document.getElementById("header-title");
+header.textContent = 'LIST OF ITEMS';
+const change = document.getElementsByClassName("title");
+change.textContent = "Add List Of Items";
+header.style.color="red";
